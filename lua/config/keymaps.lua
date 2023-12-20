d3748d95f446ba6ad7794aa71e35967f06092c3d
cotyq <cotii.q@gmail.com>
2023-12-20 18:13:50 -0300

    Telescope live_grep in custom folder

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+
+local telescope = require("telescope.builtin")
+function folder_live_grep(folder)
+  telescope.live_grep({
+    prompt_title = "Live Grep in Folder " .. folder,
+    search_dirs = { folder },
+  })
+end
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>(",
+  [[:lua folder_live_grep(vim.fn.input("Folder: "))<CR>]],
+  { noremap = true, silent = true }
+)
